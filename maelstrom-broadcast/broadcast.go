14518729c91f4ae9d58db4b82aa9c7ee408519f6
jborkows <jacek.borkowskiit@gmail.com>
2023-03-09 00:52:13 +0100

    I don't know how is performance of it -> but using go routine's with timeouts and timers

--- maelstrom-broadcast/broadcast.go
+++ maelstrom-broadcast/broadcast.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -15,6 +16,32 @@ type BroadcastResponse struct {
 	Type string `json:"type,omitempty"`
 }
 
+func gossip_broadcast(n *maelstrom.Node) func(body BroadcastRequest, destination SystemNode) {
+	return func(body BroadcastRequest, destination SystemNode) {
+		fetching_completed := make(chan bool, 1)
+		go func() {
+			n.RPC(destination, BroadcastRequest{
+				Type:    "broadcast",
+				Message: body.Message,
+			}, func(msg maelstrom.Message) error {
+				fetching_completed <- true
+				return nil
+			})
+		}()
+		select {
+		case <-time.After(100 * time.Millisecond):
+			aBody := body
+			aDestination := destination
+			go func() {
+				timer := time.NewTimer(100 * time.Millisecond)
+				<-timer.C
+				gossip_broadcast(n)(aBody, aDestination)
+			}()
+		case <-fetching_completed:
+		}
+	}
+}
+
 func Broadcast(n *maelstrom.Node) func(msg maelstrom.Message) error {
 	return func(msg maelstrom.Message) error {
 		var body BroadcastRequest
@@ -24,16 +51,12 @@ func Broadcast(n *maelstrom.Node) func(msg maelstrom.Message) error {
 
 		saved := StoreInstance().Save(body.Message)
 		if saved {
+			broadcaster := gossip_broadcast(n)
 			destinations := GossipTo(msg.Dest)
-			for i := 0; i < len(destinations); i++ {
-				n.RPC(destinations[i], BroadcastRequest{
-					Type:    "broadcast",
-					Message: body.Message,
-				}, func(msg maelstrom.Message) error {
-					return nil
-				})
-
+			for _, destination := range destinations {
+				broadcaster(body, destination)
 			}
+
 		}
 		response := BroadcastResponse{
 			Type: "broadcast_ok",
